56fcb19a5c648d3c94ee0e0d52cda5d5a95ef59f
Sri-MERN <nmsrikanth02@gmail.com>
2023-08-02 20:47:22 +0530

    n

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
 // export const  API="https://socialmedia-api-s52d.onrender.com"
-export const API="https://social-media-manager.onrender.com"
+export const API="https://social-media-management-itks.onrender.com"
